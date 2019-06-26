2a8b7bb0769f2c1fb02114752dc297a3df5c4d90
pratik <pratik17100@iiitnr.edu.in>
2019-06-26 10:20:30 +0530

    formatting

--- src/main/java/com/drupal/services/FileStorageProperties.java
+++ src/main/java/com/drupal/services/FileStorageProperties.java
@@ -6,67 +6,67 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "file")
 @Component
 public class FileStorageProperties {
-    
-/**
-* Stores the path where uploaded questions of the user are to be saved
-*/
-private String uploadDir;
-    /**
-     * Stores the path where profile picture of the user is to be saved
-     */
-    private String profilePicDir;
-    /**
-     * Stores the path where answers of the user are to be saved
-     */
-    private String answersDir;
-    
-    /**
-     * Path where the answers of the user are stored is returned
-     * 
-     * @returns path to stored answers
-     */
-    public String getAnswersDir() {
-return answersDir;
-}
 
-/**
-* Sets path where the answers of the user are to be stored
-*  
-* @param answersDir takes path where answers are stored
-*/
-public void setAnswersDir(String answersDir) {
-this.answersDir = answersDir;
-}
+	/**
+	 * Stores the path where uploaded questions of the user are to be saved
+	 */
+	private String uploadDir;
+	/**
+	 * Stores the path where profile picture of the user is to be saved
+	 */
+	private String profilePicDir;
+	/**
+	 * Stores the path where answers of the user are to be saved
+	 */
+	private String answersDir;
 
-/**
-* @returns path to the profile picture
-*/
-public String getProfilePicDir() {
-return profilePicDir;
-}
+	/**
+	 * Path where the answers of the user are stored is returned
+	 * 
+	 * @returns path to stored answers
+	 */
+	public String getAnswersDir() {
+		return answersDir;
+	}
 
-/**
-* Sets path where profile pictures are to be stored
-* 
-* @param profilePicDir takes path where profile picture gets stored
-*/
-public void setProfilePicDir(String profilePicDir) {
-this.profilePicDir = profilePicDir;
-}
+	/**
+	 * Sets path where the answers of the user are to be stored
+	 * 
+	 * @param answersDir takes path where answers are stored
+	 */
+	public void setAnswersDir(String answersDir) {
+		this.answersDir = answersDir;
+	}
 
-/**
-* @returns path to the uploaded questions of the user
-*/
-public String getUploadDir() {
-        return uploadDir;
-    }
+	/**
+	 * @returns path to the profile picture
+	 */
+	public String getProfilePicDir() {
+		return profilePicDir;
+	}
 
-    /**
-     * Sets path where question videos are to be stored
-     * 
-     * @param uploadDir takes path where question videos are stored
-     */
-    public void setUploadDir(String uploadDir) {
-        this.uploadDir = uploadDir;
-    }
+	/**
+	 * Sets path where profile pictures are to be stored
+	 * 
+	 * @param profilePicDir takes path where profile picture gets stored
+	 */
+	public void setProfilePicDir(String profilePicDir) {
+		this.profilePicDir = profilePicDir;
+	}
+
+	/**
+	 * @returns path to the uploaded questions of the user
+	 */
+	public String getUploadDir() {
+		return uploadDir;
+	}
+
+	/**
+	 * Sets path where question videos are to be stored
+	 * 
+	 * @param uploadDir takes path where question videos are stored
+	 */
+	public void setUploadDir(String uploadDir) {
+		this.uploadDir = uploadDir;
+	}
 }
